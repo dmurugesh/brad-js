00b233187289c5f5e5a42a20f19f4e6cf7a81472
dmurugesh <dmurugeshkhanna@gmail.com>
2020-07-30 12:06:58 +0530

    chapter 2 upto page 50

---
+++ 26-task46-headfirst-book/02chapter/battleship.js
@@ -0,0 +1,16 @@
+
+//Assiging the variables 
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number 0-6)");
+    console.log(guess);
+}
