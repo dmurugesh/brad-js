56ec6e943149824f15d24f527be6d8094d60916e
dmurugesh <dmurugeshkhanna@gmail.com>
2020-08-02 21:52:48 +0530

    Headfirst Chapter 5

---
+++ 26-task46-headfirst-book/05chapter/test.js
@@ -0,0 +1,24 @@
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+   };
+
+document.write(chevy.make);
+document.write("<br>");
+document.write(chevy.model);
+
+//Excersie
+var cadi = { 
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+};
