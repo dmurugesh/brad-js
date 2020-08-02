8924a77b9f9c4192fc6faaba22e3e853ca7cf846
dmurugesh <dmurugeshkhanna@gmail.com>
2020-08-02 23:53:47 +0530

    chapter 5 page 193

--- 26-task46-headfirst-book/05chapter/test.js
+++ 26-task46-headfirst-book/05chapter/test.js
@@ -22,3 +22,48 @@ var cadi = {
     convertible: false,
     mileage: 12892
 };
+
+// Code 
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2, 
+    convertible: false,
+    mileage: 88000,
+    a: "50",
+    b: "6"
+}
+
+// fiat.mileage = 10000;
+var miles = fiat.mileage;
+if (miles > 2000) {
+ buyIt();
+}
+
+function buyIt() {
+let x = (fiat.a - fiat.b);
+console.log("value :",x);
+}
+
+//Adding property to object 
+fiat.needsWashing = true;
+console.log(fiat.needsWashing);
+
+//Object magnets 
+var dog = {
+    name: "Fido",
+    weight: 20.2,
+    age: 4,
+    breed: "mixed",
+    activity: "fetch balls"
+   };
+   var bark;
+   if (dog.weight > 20) {
+    bark = "WOOF WOOF";
+   } else {
+    bark = "woof woof";
+   }
+   var speak = dog.name + " says " + bark + " when he wants to " + dog.activity;
+   console.log(speak);
