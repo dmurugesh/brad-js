7205b9dccf5708a2f741f1a4b0b971573022ced6
dmurugesh <dmurugeshkhanna@gmail.com>
2020-08-05 21:15:00 +0530

    chapter 7 completed

--- 26-task46-headfirst-book/07chapter/test.js
+++ 26-task46-headfirst-book/07chapter/test.js
@@ -277,3 +277,12 @@ function validateAmy(phoneNumber) {
     return ("Amy : " + phoneNumber.match(/^\d{3}-?\d{4}$/));
 }
 console.log(validateAmy(phoneNumber));
+
+function Duck(sound) {
+    this.sound = sound;
+    this.quack = function () { console.log(this.sound); }
+}
+var toy = new Duck("quack quack");
+toy.quack();
+console.log(typeof toy);
+console.log(toy instanceof Duck);
