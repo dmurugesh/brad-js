6a17e9ee54d03b0181286e5d6b690cedd181656f
dmurugesh <dmurugeshkhanna@gmail.com>
2020-08-12 15:45:10 +0530

    Chapter 11 upto 477

---
+++ 26-task46-headfirst-book/11chapter/chapter11.js
@@ -0,0 +1,6 @@
+//Sample Function
+function handler() { alert("Yeah, that page loaded!"); }
+window.onload = handler;
+
+//annonyms function
+window.onload = function() { alert("Yeah, that page loaded!"); };
