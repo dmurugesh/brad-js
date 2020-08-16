da1b2d2170753fddb8ee624e57864d33f0fd3696
dmurugesh <dmurugeshkhanna@gmail.com>
2020-08-16 22:15:38 +0530

    Chapter 13 upto 584

--- 26-task46-headfirst-book/13chapter/13chapter.js
+++ 26-task46-headfirst-book/13chapter/13chapter.js
@@ -52,42 +52,84 @@ spot.wag();
 
 var barnaby = new Dog("Barnaby", "Basset Hound", 55);
 
+Dog.prototype.sitting = false;
 Dog.prototype.sit = function () {
-    console.log(this.name + " is now sitting");
-}
+    if (this.sitting) {
+        console.log(this.name + " is already sitting");
+    } else {
+        this.sitting = true;
+        console.log(this.name + " is now sitting");
+    }
+};
 barnaby.sit();
+barnaby.sit()
+barnaby.sit()
+spot.sit()
+spot.sit()
 
-function Robot(name, year, owner) {
-    this.name = name;
-    this.year = year;
-    this.owner = owner;
-}
-Robot.prototype.maker = "ObjectsRUs";
-Robot.prototype.speak = function () {
-    console.log("speak");
-};
-Robot.prototype.makeCoffee = function () {
-    console.log("make coffee")
-};
-Robot.prototype.blinkLights = function () {
-    console.log("blink blink");
-};
-var robby = new Robot("Robby", 1956, "Dr. Morbius");
-var rosie = new Robot("Rosie", 1962, "George Jetson");
-robby.onOffSwitch = true;
-robby.makeCoffee = function () {
-    console.log("Fetching a coffee from Starbucks.")
-};
-rosie.cleanHouse = function () {
-    console.log("Clean the house")
-};
+// //Page 579
 
-console.log(robby.name + " was made by " + robby.maker +
-    " in " + robby.year + " and is owned by " + robby.owner);
-robby.makeCoffee();
-robby.blinkLights();
+// function Robot(name, year, owner) {
+//     this.name = name;
+//     this.year = year;
+//     this.owner = owner;
+// }
+// Robot.prototype.maker = "ObjectsRUs";
+// Robot.prototype.speak = function () {
+//     console.log("speak");
+// };
+// Robot.prototype.makeCoffee = function () {
+//     console.log("make coffee")
+// };
+// Robot.prototype.blinkLights = function () {
+//     console.log("blink blink");
+// };
+// var robby = new Robot("Robby", 1956, "Dr. Morbius");
+// var rosie = new Robot("Rosie", 1962, "George Jetson");
+// robby.onOffSwitch = true;
+// robby.makeCoffee = function () {
+//     console.log("Fetching a coffee from Starbucks.")
+// };
+// rosie.cleanHouse = function () {
+//     console.log("Clean the house")
+// };
 
-console.log(rosie.name + " was made by " + rosie.maker +
-    " in " + rosie.year + " and is owned by " + rosie.owner);
-rosie.cleanHouse();
+// console.log(robby.name + " was made by " + robby.maker +
+//     " in " + robby.year + " and is owned by " + robby.owner);
+// robby.makeCoffee();
+// robby.blinkLights();
 
+// console.log(rosie.name + " was made by " + rosie.maker +
+//     " in " + rosie.year + " and is owned by " + rosie.owner);
+// rosie.cleanHouse();
+
+
+// function Game() {
+//     this.level = 0;
+// }
+// Game.prototype.play = function () {
+//     // player plays game here
+//     this.level++;
+//     console.log("Welcome to level " + this.level);
+//     this.unlock();
+// }
+// Game.prototype.unlock = function () {
+//     if (this.level === 42) {
+//         Robot.prototype.deployLaser = function () {
+//             console.log(this.name + " is blasting you with laser beams.");
+//         }
+//     }
+// }
+// function Robot(name, year, owner) {
+//     this.name = name;
+//     this.year = year;
+//     this.owner = owner;
+// }
+// var game = new Game();
+// var robby = new Robot("Robby", 1956, "Dr. Morbius");
+// var rosie = new Robot("Rosie", 1962, "George Jetson");
+// while (game.level < 42) {
+//     game.play();
+// }
+// robby.deployLaser();
+// rosie.deployLaser();
