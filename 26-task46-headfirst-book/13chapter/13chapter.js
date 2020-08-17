d8bcb72c832a4b4b85cdcf2c4ba8c608d666224b
dmurugesh <dmurugeshkhanna@gmail.com>
2020-08-17 15:40:03 +0530

    Chapter 13 completed

--- 26-task46-headfirst-book/13chapter/13chapter.js
+++ 26-task46-headfirst-book/13chapter/13chapter.js
@@ -40,15 +40,15 @@ spot.bark = function () {
     console.log(this.name + " says WOOF!");
 };
 
-fido.bark();
-fido.run();
-fido.wag();
-fluffy.bark();
-fluffy.run();
-fluffy.wag();
-spot.bark();
-spot.run();
-spot.wag();
+// fido.bark();
+// fido.run();
+// fido.wag();
+// fluffy.bark();
+// fluffy.run();
+// fluffy.wag();
+// spot.bark();
+// spot.run();
+// spot.wag();
 
 var barnaby = new Dog("Barnaby", "Basset Hound", 55);
 
@@ -61,11 +61,116 @@ Dog.prototype.sit = function () {
         console.log(this.name + " is now sitting");
     }
 };
-barnaby.sit();
-barnaby.sit();
-barnaby.sit();
-spot.sit();
-spot.sit();
+// barnaby.sit();
+// barnaby.sit();
+// barnaby.sit();
+// spot.sit();
+// spot.sit();
+
+ShowDog.prototype = new Dog();
+ShowDog.prototype.constructor = ShowDog;
+
+// var aDog = new Dog();
+
+function ShowDog(name, breed, weight, handler) {
+    Dog.call(this, name, breed, weight);
+    this.handler = handler;
+}
+
+ShowDog.prototype.league = "Webville";
+ShowDog.prototype.stack = function () {
+    console.log("Stack");
+};
+ShowDog.prototype.bait = function () {
+    console.log("Bait");
+};
+ShowDog.prototype.gait = function (kind) {
+    console.log(kind + "ing");
+};
+ShowDog.prototype.groom = function () {
+    console.log("Groom");
+};
+var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+// console.log(scotty);
+
+// scotty.stack();
+// scotty.bark();
+// console.log(scotty.league);
+// console.log(scotty.species);
+
+
+var fido = new Dog("Fido", "Mixed", 38);
+if (fido instanceof Dog) {
+    console.log("Fido is a Dog");
+}
+if (fido instanceof ShowDog) {
+    console.log("Fido is a ShowDog");
+}
+var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+if (scotty instanceof Dog) {
+    console.log("Scotty is a Dog");
+}
+if (scotty instanceof ShowDog) {
+    console.log("Scotty is a ShowDog");
+}
+// console.log("Fido constructor is " + fido.constructor);
+// console.log("Scotty constructor is " + scotty.constructor);
+
+// console.log(scotty);
+
+var fido = new Dog("Fido", "Mixed", 38);
+var fluffy = new Dog("Fluffy", "Poodle", 30);
+var spot = new Dog("Spot", "Chihuahua", 10);
+var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+var beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
+fido.bark();
+fluffy.bark();
+spot.bark();
+scotty.bark();
+beatrice.bark();
+scotty.gait("Walk");
+beatrice.groom();
+
+
+function Robot(name, year, owner) {
+    this.name = name;
+    this.year = year;
+    this.owner = owner;
+}
+Robot.prototype.toString = function () {
+    return this.name + " Robot belonging to " + this.owner;
+};
+var toy = new Robot("Toy", 2013, "Avary");
+console.log(toy.toString());
+
+console.log("Robot is: " + toy);
+
+String.prototype.cliche = function () {
+    var cliche = ["lock and load", "touch base", "open the kimono"];
+    for (var i = 0; i < cliche.length; i++) {
+        var index = this.indexOf(cliche[i]);
+        if (index >= 0) {
+            return true;
+        }
+    }
+    return false;
+};
+
+var sentences = ["I'll send my car around to pick you up.",
+    "Let's touch base in the morning and see where we are",
+    "We don't want to open the kimono, we just want to inform them."];
+for (var i = 0; i < sentences.length; i++) {
+    var phrase = sentences[i];
+    if (phrase.cliche()) {
+        console.log("CLICHE ALERT: " + phrase);
+    }
+}
+
+String.prototype.palindrome = function() {
+    var r = this.split("").reverse().join("");
+    return (r === this.valueOf());
+}
+
 
 // //Page 579
 
